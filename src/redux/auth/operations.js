e04a9d713d891fa1421754e41a9b1ebe977f9786
DanBeliaev <beliaev1108@gmail.com>
2024-09-30 20:28:00 +0100

    finnnn

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,9 +1,8 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-axios.defaults.baseURL = "https://connections-api.goit.global/";
+axios.defaults.baseURL = "https://connections-api.goit.global";
 
-axios.defaults.withCredentials = true;
 
 const setAuthHeader = (token) => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
